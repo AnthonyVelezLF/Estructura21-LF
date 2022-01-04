12a3a4b21861c664d73568a0cc9add23584541c4
Anth8ny LF <86171664+AnthonyVelezLF@users.noreply.github.com>
2022-01-04 18:57:20 -0500

    Delete ejercicio15.py

--- ejercicio15.py
+++
@@ -1,8 +0,0 @@
-#operadores logicos en python, nos permiten comparar dos expresiones
-#entrada
-expresion1 = True
-expresion2 = False
-#proceso
-resultado = expresion1 and expresion2
-#Salida
-print(resultado)
