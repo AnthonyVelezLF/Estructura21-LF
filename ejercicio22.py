572b8109493dda7b436c306ac9251fb29d954575
Anth8ny LF <86171664+AnthonyVelezLF@users.noreply.github.com>
2022-01-04 18:54:53 -0500

    Delete ejercicio22.py

--- ejercicio22.py
+++
@@ -1,9 +0,0 @@
-#bla bla bla 
-#Emtrada
-numero = int(input("Ingrese un numero: "))
-antecesor = numero - 1
-#proceso
-while antecesor > 0:
-    if antecesor  % 2==0:
-        print((antecesor))
-    antecesor = antecesor-1
