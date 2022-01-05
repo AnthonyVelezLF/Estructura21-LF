0504053ea3b603c048643b0eea75b4e9978f742a
Anth8ny LF <86171664+AnthonyVelezLF@users.noreply.github.com>
2022-01-04 19:07:47 -0500

    Add files via upload

---
+++ e30.py
@@ -0,0 +1,2 @@
+#Ejercicio 30
+#Construye un algoritmo que calcule e imprima la tabla de multiplicar, desde la tabla del 1 hasta la del 10.
---
+++ e31.py
@@ -0,0 +1,5 @@
+#ejercicio31
+#Escribir un algoritmo que muestre todas las fichas de dominó, sin repetir. 
+print("ejrcicio18")
+h=int(input("ingrese numero de horas a estacionar: "))
+mp = h * 4 
